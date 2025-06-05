61a2a42f406e3c1d923e94c22272276fb44e22d9
yechan47 <163622092+yechan47@users.noreply.github.com>
2025-06-05 18:31:07 +0900

    [Bronze I] Title: 더하기 사이클, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.cc
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+main() 
+{
+	int N;
+	int temp;
+	scanf("%d", &N);
+
+	if (N < 10) 
+        N *= 10;
+
+	int cnt = 0;
+	temp = N;
+
+	while (1) 
+    {
+		cnt++;
+		temp = temp % 10 * 10 + (temp / 10 + temp % 10) % 10;
+
+		if (temp == N) 
+			break;
+	}
+	printf("%d", cnt);
+}
