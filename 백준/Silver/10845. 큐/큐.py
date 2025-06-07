d2345e02af1cf679e1e9a50e0f79f3a58a79e1f8
yechan47 <163622092+yechan47@users.noreply.github.com>
2025-06-07 17:47:44 +0900

    [Silver IV] Title: 큐, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,37 @@
+import sys
+
+n = int(sys.stdin.readline())
+q = []
+
+for i in range(n):
+    a = sys.stdin.readline().split()
+
+    if a[0] == "push":
+        q.insert(0, a[1])
+
+    elif a[0] == "pop":
+        if len(q) != 0:
+            print(q.pop())
+        else:
+            print(-1)
+
+    elif a[0] == "size":
+        print(len(q))
+
+    elif a[0] == "empty":
+        if len(q) == 0:
+            print(1)
+        else:
+            print(0)
+
+    elif a[0] == "front":
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[len(q) - 1])
+
+    elif a[0] == "back":
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[0])
