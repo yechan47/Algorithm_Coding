e5aef9c8ee326d3af0930baaff246562a5600b41
yechan47 <163622092+yechan47@users.noreply.github.com>
2025-06-07 18:10:04 +0900

    [Silver IV] Title: solved.ac, Time: 152 ms, Memory: 35868 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -0,0 +1,22 @@
+import sys
+
+def my_round(num):
+    if num - int(num) >= 0.5:
+        return int(num) + 1
+    else:
+        return int(num)
+
+if __name__ == '__main__':
+    n = int(sys.stdin.readline())
+    if not n:
+        print(0)
+    else:
+        difficulty = [int(sys.stdin.readline()) for _ in range(n)]
+        difficulty.sort()
+        exception = my_round(n * 0.15)
+        
+        if exception != 0:
+            difficulty = difficulty[exception: -exception]
+        cnt = n - 2 * exception
+        result = sum(difficulty)
+        print(my_round(result / cnt))
