3298c776b55b77219eba65cd5dc28405cb220f39
yechan47 <163622092+yechan47@users.noreply.github.com>
2025-06-07 17:50:48 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main()
+{
+    int N;
+    scanf("%d", &N);
+    
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < 2*N; j++)
+            putchar((i+j)%2 ? 32 : '*');
+        putchar(10);
+    }
+}
