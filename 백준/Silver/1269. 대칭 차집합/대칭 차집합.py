04d0fd2007318fda031810479d6fba3ab121e98a
yechan47 <163622092+yechan47@users.noreply.github.com>
2025-06-27 19:34:01 +0900

    [Silver IV] Title: 대칭 차집합, Time: 228 ms, Memory: 89768 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+a, b=map(int, input().split())
+
+a_list = set(map(int, input().split()))
+b_list = set(map(int, input().split()))
+
+print(len(a_list ^ b_list))
