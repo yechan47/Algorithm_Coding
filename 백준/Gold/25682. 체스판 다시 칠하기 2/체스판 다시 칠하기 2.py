a66a25a0156f943c9bc16950ec0428030e0da066
yechan47 <163622092+yechan47@users.noreply.github.com>
2025-07-23 19:20:15 +0900

    [Gold IV] Title: 체스판 다시 칠하기 2, Time: 564 ms, Memory: 300140 KB -BaekjoonHub

---
+++ 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+N, M, K = map(int, input().split())
+
+maps = [list(input().rstrip()) for _ in range(N)]
+
+SUM = [[0] * (M+1) for _ in range(N+1)]
+
+for r in range(1, N+1):
+    for c in range(1, M+1):
+        if (r + c) % 2 == 0:
+            if maps[r-1][c-1] == 'B':
+                SUM[r][c] = SUM[r-1][c] + SUM[r][c-1] - SUM[r-1][c-1]
+            else:
+                SUM[r][c] = SUM[r-1][c] + SUM[r][c-1] - SUM[r-1][c-1] + 1
+        else:
+            if maps[r-1][c-1] == 'W':
+                SUM[r][c] = SUM[r-1][c] + SUM[r][c-1] - SUM[r-1][c-1]
+            else:
+                SUM[r][c] = SUM[r-1][c] + SUM[r][c-1] - SUM[r-1][c-1] + 1
+
+max_ = -float('inf')
+min_ = float('inf')
+for r in range(K, N+1):
+    for c in range(K, M+1):
+        max_ = max(SUM[r][c] - SUM[r-K][c] - SUM[r][c-K] + SUM[r-K][c-K], max_)
+        min_ = min(SUM[r][c] - SUM[r-K][c] - SUM[r][c-K] + SUM[r-K][c-K], min_)
+
+print(min(min_, max_, K*K - min_, K*K - max_))
