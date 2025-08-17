0e7e5a65fd31fe440f52f4e31d6fc93cf77fe7c4
yechan47 <163622092+yechan47@users.noreply.github.com>
2025-08-17 19:41:31 +0900

    [Silver I] Title: 안전 영역, Time: 700 ms, Memory: 34992 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+n = int(input())
+graph = []
+max_height = 0
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+    max_height = max(max_height, max(graph[-1]))
+    
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+    
+def bfs(x, y, r):
+    queue = deque()
+    queue.append((x, y))
+    visited[x][y] = 1
+    
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx<0 or nx>=n or ny<0 or ny>=n:
+                continue
+            if visited[nx][ny] == 1 or graph[nx][ny] <= r:
+                continue
+            queue.append((nx, ny))
+            visited[nx][ny] = 1
+
+result = 0
+for i in range(max_height):
+    count = 0
+    visited = [[0]*n for _ in range(n)]
+    for j in range(n):
+        for k in range(n):
+            if graph[j][k] > i and visited[j][k] == 0:
+                bfs(j, k, i)
+                count += 1
+    result = max(result, count)
+    
+print(result)
